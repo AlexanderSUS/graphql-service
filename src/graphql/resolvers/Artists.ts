44250e8e7ed9d5f9adbc0d368ebd5c185c6a646b
Alexander Suslov <sasha_suslov07@mail.ru>
2022-07-07 10:52:53 +0300

    feat: add error handling for invalid artist ID case to updateArtist mutation

--- src/graphql/resolvers/Artists.ts
+++ src/graphql/resolvers/Artists.ts
@@ -1,3 +1,4 @@
+import { UserInputError } from 'apollo-server-core';
 import { Resolvers } from '../../types/resolvers';
 import { Artist, CreateArtistArgs, UpdateArtistArgs } from '../../types/artists';
 import { List } from '../../types/list';
@@ -8,30 +9,35 @@ const artistsResolver: Resolvers = {
   Artist: {
     async bands(artist: Artist, args: any, { dataSources }) {
       const bands = await dataSources.bandsAPI.getBands() as List<Band>;
+
       return artist.bandsIds.map((id) => bands.items.find((band) => band._id === id));
     },
   },
 
   Query: {
-    async artists(_: any, queryParams: QueryParams, { dataSources }) {
+    artists(_: any, queryParams: QueryParams, { dataSources }) {
       return dataSources.artistsAPI.getArtists(queryParams);
     },
 
-    async artist(_: any, { id }: { id : string }, { dataSources }) {
+    artist(_: any, { id }: { id : string }, { dataSources }) {
       return dataSources.artistsAPI.getArtist(id);
     },
   },
 
   Mutation: {
-    async createArtist(_: any, args: CreateArtistArgs, { dataSources }) {
+    createArtist(_: any, args: CreateArtistArgs, { dataSources }) {
       return dataSources.artistsAPI.createArtist(args);
     },
 
     async updateArtist(_: any, args: UpdateArtistArgs, { dataSources }) {
-      return dataSources.artistsAPI.updateArtist(args);
+      const artist = await dataSources.artistsAPI.updateArtist(args) as Artist;
+
+      if (!artist._id) throw new UserInputError('Bad input, check Ids');
+
+      return artist;
     },
 
-    async deleteArtist(_: any, { id }: { id : string }, { dataSources }) {
+    deleteArtist(_: any, { id }: { id : string }, { dataSources }) {
       return dataSources.artistsAPI.deleteArtist(id);
     },
   },
