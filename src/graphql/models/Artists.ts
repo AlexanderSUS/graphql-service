9aaf2a5e3ab20619fc02ccb62d85d99304c1dc04
Alexander Suslov <sasha_suslov07@mail.ru>
2022-07-07 16:34:31 +0300

    fix: arist mutation

--- src/graphql/models/Artists.ts
+++ src/graphql/models/Artists.ts
@@ -31,8 +31,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    createArtist(firstName: String, secondName: String, middleName: String, birthDate: String, birthPlace: String, country: String, bands: [ID], instruments: [String]): Artist
-    updateArtist(id: ID!, firstName: String, secondName: String, middleName: String, birthDate: String, birthPlace: String, country: String, bands: [ID], instruments: [String]): Artist
+    createArtist(firstName: String, secondName: String, middleName: String, birthDate: String, birthPlace: String, country: String, bandsIds: [ID], instruments: [String]): Artist
+    updateArtist(id: ID!, firstName: String, secondName: String, middleName: String, birthDate: String, birthPlace: String, country: String, bandsIds: [ID], instruments: [String]): Artist
     deleteArtist(id: ID!): DeleteArtistReturnObject 
   }
 `;
