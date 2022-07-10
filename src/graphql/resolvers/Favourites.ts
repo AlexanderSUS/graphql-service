f7bd2e312ccd97758be26516b4ea59cbf6ee0938
Alexander Suslov <sasha_suslov07@mail.ru>
2022-07-10 11:49:59 +0300

    fix: fix addGenreToFavourites Mutation

--- src/graphql/resolvers/Favourites.ts
+++ src/graphql/resolvers/Favourites.ts
@@ -1,31 +1,40 @@
 import { Resolvers } from '../../types/resolvers';
 import { Favourites } from '../../types/favourites';
 import { QueryParams } from '../../types/queryParams';
+import filterByExistance from '../../utils/firlterByExistanse';
 
 const favouritesResolver: Resolvers = {
   Favourites: {
-    genres(favourites: Favourites, args: any, { dataSources }) {
-      return Promise.all(
+    async genres(favourites: Favourites, args: any, { dataSources }) {
+      const genres = await Promise.all(
         favourites.genresIds.map((genreId) => dataSources.genresAPI.getGenre(genreId)),
       );
+
+      return filterByExistance(genres);
     },
 
-    bands(favourites: Favourites, args: any, { dataSources }) {
-      return Promise.all(
+    async bands(favourites: Favourites, args: any, { dataSources }) {
+      const bands = await Promise.all(
         favourites.bandsIds.map((bandsId) => dataSources.bandsAPI.getBand(bandsId)),
       );
+
+      return filterByExistance(bands);
     },
 
-    artists(favourites: Favourites, args: any, { dataSources }) {
-      return Promise.all(
+    async artists(favourites: Favourites, args: any, { dataSources }) {
+      const artists = await Promise.all(
         favourites.artistsIds.map((artistId) => dataSources.artistsAPI.getArtist(artistId)),
       );
+
+      return filterByExistance(artists);
     },
 
-    tracks(favourites: Favourites, args: any, { dataSources }) {
-      return Promise.all(
+    async tracks(favourites: Favourites, args: any, { dataSources }) {
+      const tracks = await Promise.all(
         favourites.tracksIds.map((trackId) => dataSources.tracksAPI.getTrack(trackId)),
       );
+
+      return filterByExistance(tracks);
     },
   },
 
@@ -49,7 +58,7 @@ const favouritesResolver: Resolvers = {
     },
 
     addGenreToFavourites(_: any, { genresId }: { genresId: string }, { dataSources }) {
-      return dataSources.favouritesAPI.addArtistToFavourites(genresId);
+      return dataSources.favouritesAPI.addGenreToFavourites(genresId);
     },
   },
 };
